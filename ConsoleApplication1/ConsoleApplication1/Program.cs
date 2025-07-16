4a8313d1cd26ed8c22a2553ed710bfec4e992ca2
jegan <jjegatheeswaran05@gmail.com>
2025-07-16 10:33:01 +0530

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ENTER THE NUMBER:");
+            int NUM = int.Parse(Console.ReadLine());
+            int MINDIV = 2;
+            int MAXDIV = (int)Math.Sqrt(NUM);
+            bool PRIME = true;
+            while (PRIME && MINDIV <= MAXDIV)
+            {
+                if (NUM % MINDIV == 0)
+                {
+                    PRIME = false;
+                }
+                MINDIV++;
+            }
+            Console.Write("is THE NUMBER  PRIME?"+(NUM ==1? !PRIME:PRIME));
+            Console.ReadLine();
+        }           
+    }
+}
